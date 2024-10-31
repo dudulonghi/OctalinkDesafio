daa1908d24f20d45c7ea693be211109e9cc26fff
Eduardo L <eduardolonghi@hotmail.com>
2024-10-30 22:59:29 -0300

    Created components without functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFilterComponent } from './Components/search-filter/search-filter.component';
+import { ModalAddComponent } from './Components/modal-add/modal-add.component';
+import { DeleteModalComponent } from './Components/delete-modal/delete-modal.component';
+import { ItemTableComponent } from './Components/item-table/item-table.component';
+import { PaginationComponent } from './Components/pagination/pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFilterComponent,
+    ModalAddComponent,
+    DeleteModalComponent,
+    ItemTableComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
